fd0fe5ed907b34f498c48fbbc9e409b7db841341
Harshit Batra <payafterwork.in@gmail.com>
2019-08-20 19:22:49 +0530

    Added component and resturtured files

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
     <Navbar />
+    <GMap />
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/layout/Navbar'
+import GMap from '@/components/home/GMap'
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar, GMap
   }
 }
 </script>
