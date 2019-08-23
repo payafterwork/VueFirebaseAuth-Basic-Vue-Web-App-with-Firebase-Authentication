7877792ee35da65c51107cae26ba5d3fb54b21dd
Harshit Batra <payafterwork.in@gmail.com>
2019-08-23 18:18:26 +0530

    Conditional Showing Of Navbar Links

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -6,9 +6,10 @@
          Firebase Auth
         </router-link>
         <ul class="right">
-          <li ><router-link :to="{name:'Signup'}">Signup</router-link></li>
-             <li ><router-link :to="{name:'Login'}">Login</router-link></li>
-          <li><a @click="logout">Logout</a></li>
+          <li v-if="!user"><router-link :to="{name:'Signup'}">Signup</router-link></li>
+           <li v-if="!user" ><router-link :to="{name:'Login'}">Login</router-link></li>
+          <li v-if="user">{{user.email}}</li>
+          <li v-if="user"><a @click="logout">Logout</a></li>
         </ul>
       </div>
     </nav>
@@ -21,6 +22,7 @@ export default {
   name: 'Navbar',
   data(){
     return{
+      user: null
     }
   },methods:{
     logout(){
@@ -29,7 +31,16 @@ export default {
 
       })
     }
-  }
+  },created(){
+    // let user = firebase.auth().currentUser
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user){
+        this.user = user
+      } else {
+        this.user = null
+      }
+    })    
+  },
 }
 </script>
 
